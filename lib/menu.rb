9837b69262fb1a52cc3e8c54ba36adfc34986e49
Cristian Fabricio Rodriguez Romero <fab7696650@gmail.com>
2025-04-03 10:33:40 -0500

    Add exceptions
    It adds message exceptions in the main menu.

--- lib/menu.rb
+++ lib/menu.rb
@@ -7,13 +7,27 @@ class Menu
       puts 'Este es un programa para extraer información de imagenes, archivos, etc.'
       CLI::UI::Prompt.ask('Por favor seleccione una opcion para continuar') do |handler|
         handler.option('Extraer texto desde imagen') do |x|
-          image_path = CLI::UI::Prompt.ask('Ingresa la ruta de la imagen')
-          conversion = Conversion.new(image_path)
-          path_file = CLI::UI::Prompt.ask('Ingresa la ruta y nombre del archivo donde se colocara el texto')
-          File.write(path_file, conversion.image_to_text)
+          begin
+            image_path = CLI::UI::Prompt.ask('Ingresa la ruta de la imagen')
+            extract = Conversion.new(image_path).image_to_text
+            path_file = CLI::UI::Prompt.ask('Ingresa la ruta y nombre del archivo donde se colocara el texto')
+            File.write(path_file, extract)
+          rescue => error
+            puts "Error: #{error.message}"
+            CLI::UI::Frame.divider('')
+            self.main
+          end
         end
         # TO DO: Implement the logic for this menu
-        handler.option('Extraer texto desde pdf')
+        handler.option('Extraer texto desde pdf') do |x|
+          puts 'Aun no se ha implementado esta funcionalidad'
+          CLI::UI::Frame.divider('')
+          self.main
+        end
+
+        handler.option('Salir') do |x|
+          puts 'Bye!'
+        end
       end
     end
   end
